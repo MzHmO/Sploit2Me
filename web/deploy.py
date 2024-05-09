1ea50795fc6e54d569ffe82cd6421caa70fc6d31
Michael Zhmailo <michael.zhmailo@yandex.ru>
2024-05-09 11:22:00 +0500

    Изменение файловой структуры.

--- web/deploy.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-def deploy_web(host, port, debug):
-    
-    logging.debug(f"[*] Starting web server on {host}:{port} with debug mode in state:{True if debug else False}")
-    pass
