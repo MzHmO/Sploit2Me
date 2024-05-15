edff57f56716d378349a213d920162f0d3542cb4
pashablatt <lordazimov@yandex.ru>
2024-05-15 10:22:42 +0500

    Добавил html + css, также запуск приложения и перенес базу

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
+from flask import Flask
+
+
 class FileSystem:
     @staticmethod
     def getvullistpath(filename):
-        current_dir = os.getcwd() 
+        current_dir = os.getcwd()
         folder_name = "files"
         filename = filename
         full_path = os.path.join(current_dir, folder_name, filename)
@@ -16,3 +19,7 @@ VULNLISTPATH = FileSystem.getvullistpath(filename=VULNLISTFILENAME)
 
 TESTFILE = "vullist_test.xlsx"
 TESTFILEPATH = FileSystem.getvullistpath(filename=TESTFILE)
+
+app = Flask(__name__, template_folder=os.getcwd() + '\\templates', static_folder=os.getcwd() + '\\static')
+app.secret_key = "l0l1T'sSecretKey!@xD"
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'files')
--- main.py
+++ main.py
@@ -18,26 +18,33 @@ from web.deploy import WebServer
 from parsing.parse import Parser
 from botnotify.tg import BotService
 
+
 def start_web_server(options):
     WebServer.start(host=options.host, port=options.port, debug=options.debug)
+
 
 def start_bot_service(options):
     BotService.start(bot_token=options.token)
 
+
 def start_parser(options):
     Parser.start(timeout=5, use_debug_file=options.testfile)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    add_help=True,
-                    description=
-                        "Sploit2Me - a project for notification of users about new vulnerabilities that appear on the \"БДУ ФСТЭК\"")
+        add_help=True,
+        description=
+        "Sploit2Me - a project for notification of users about new vulnerabilities that appear on the \"БДУ ФСТЭК\"")
     parser.add_argument("-debug", action="store_true", help='Turn Debug output ON', default=False)
     parser.add_argument("-warn", action="store_true", help="Turn WARN output ON", default=False)
-    parser.add_argument("-testfile", action="store_true", help="Use test file for getting information about vulns", default=False)
-    parser.add_argument("-host", action="store", help="IP of network interface on which deploy web app on", default="127.0.0.1")
+    parser.add_argument("-testfile", action="store_true", help="Use test file for getting information about vulns",
+                        default=False)
+    parser.add_argument("-host", action="store", help="IP of network interface on which deploy web app on",
+                        default="127.0.0.1")
     parser.add_argument("-token", action="store", help="Telegram bot token for sending messages to user's", default="")
-    parser.add_argument("-port", action="store", help="From 1 to 65536 port value on which web server will be started", default=80)
+    parser.add_argument("-port", action="store", help="From 1 to 65536 port value on which web server will be started",
+                        default=8000)
 
     options = parser.parse_args()
 
@@ -49,12 +56,12 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s - %(levelname)s - %(message)s')
 
     web_server_thread = threading.Thread(target=start_web_server, args=(options,))
-    bot_service_thread = threading.Thread(target=start_bot_service, args=(options,))
-    parser_thread = threading.Thread(target=start_parser, args=(options,))
+    # bot_service_thread = threading.Thread(target=start_bot_service, args=(options,))
+    # parser_thread = threading.Thread(target=start_parser, args=(options,))
     web_server_thread.start()
-    bot_service_thread.start()
-    parser_thread.start()
-    
+    # bot_service_thread.start()
+    # parser_thread.start()
+
     web_server_thread.join()
-    bot_service_thread.join()
-    parser_thread.join()
+    # bot_service_thread.join()
+    # parser_thread.join()
