34eafd9eb0af7f7394d12260935883697229aeeb
Michael Zhmailo <michael.zhmailo@yandex.ru>
2024-05-17 18:16:14 +0500

    Изменения в нейминге
    Co-Authored-By: AkashiSeijura <117091655+akashiseijura@users.noreply.github.com>

--- web/cards.py
+++ web/cards.py
@@ -5,13 +5,13 @@ from flask_login import login_required
 from .database import Database
 
 @login_required
-def view_profile():
+def view_cards():
     try:
         if '_user_id' not in session:
             return redirect(url_for('login'))
 
         user = Database.get_user_by_id(session['_user_id'])
-        return render_template('profile.html', files=[], username=user.username)
+        return render_template('cards.html', files=[], username=user.username)
     except Exception as e:
         print(e)
-        return render_template('profile.html', files=[], username=user.username, error=str(e))
+        return render_template('cards.html', files=[], username=user.username, error=str(e))
--- web/deploy.py
+++ web/deploy.py
@@ -1,7 +1,7 @@
 import logging
 from . import forms
 from .database import Database
-from .profile import view_profile
+from .cards import view_cards
 from flask import Flask, url_for, redirect, request, render_template, flash
 from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 
@@ -16,7 +16,7 @@ login_manager.login_view = 'login'
 # EXTERNAL ROUTES
 # app.add_url_rule('/files', methods=['GET', 'POST'], view_func=file_handling)
 # app.add_url_rule('/download/<filename>', view_func=download_file)
-app.add_url_rule('/profile', methods=['GET', 'POST'], view_func=view_profile)
+app.add_url_rule('/cards', methods=['GET', 'POST'], view_func=view_cards)
 
 
 # INTERNAL ROUTES
@@ -28,7 +28,7 @@ def load_user(user_id):
 @app.route('/')
 def home():
      if current_user.is_authenticated:
-         return redirect(url_for('view_profile'))
+         return redirect(url_for('view_cards'))
      return redirect(url_for('login'))
 
 
@@ -40,7 +40,7 @@ def login():
         if Database.validate_login_by_username(username, password):
             user = Database.get_user_by_username(username)
             login_user(user)
-            return redirect(url_for('view_profile'))
+            return redirect(url_for('view_cards'))
         else:
             flash('Неверное имя пользователя или пароль')
 
